0a556f6bc277aa9e543246985d93e7afc7ff4a5e
Pedro Moreno Sanchez <pedrorechez@gmail.com>
2011-07-07 16:37:44 +0000

    Added config file's changes to ChangeLog file
    Deleted config warnings
    
    Added header to config files.

--- src/loadconfig.c
+++ src/loadconfig.c
@@ -1,14 +1,27 @@
-/**
- * section: Tree
- * synopsis: Navigates a tree to print element names
- * purpose: Parse a file to a tree, use xmlDocGetRootElement() to
- *          get the root element, then walk the document and print
- *          all the element name in document order.
- * usage: tree1 filename_or_URL
- * test: tree1 test2.xml > tree1.tmp ; diff tree1.tmp tree1.res ; rm tree1.tmp
- * author: Dodji Seketeli
- * copy: see Copyright for the status of this software.
+/*
+ *  loadconfig.c
+ *  
+ * 	Contains functions wich performs the parser xml file for the PaC and PAA.
+ *
+ *  Copyright (C) Pedro Moreno Sánchez & Francisco Vidal Meca on 06/07/10.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  
+ *  https://sourceforge.net/projects/openpana/
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -64,10 +77,10 @@ parse_xml_client(xmlNode * a_node)
 			else if (strcmp(cur_node->name, "PORT")==0){
 				if (paa){
 
-					sscanf(xmlNodeGetContent(cur_node), "%d", &DSTPORT);
+					sscanf(xmlNodeGetContent(cur_node), "%hd", &DSTPORT);
 				}
 				else if (pac) {
-					sscanf(xmlNodeGetContent(cur_node), "%d", &SRCPORT);
+					sscanf(xmlNodeGetContent(cur_node), "%hd", &SRCPORT);
 				}
 			}
 			else if (strcmp(cur_node->name, "TIMEOUT")==0){
